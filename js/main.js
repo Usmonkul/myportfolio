6028d4b0cc8f255e818b93ce2304f4a200ad6084
Usmon <usmonhakimov1999@gmail.com>
2022-06-21 14:17:24 +0900

    add media queries for mobile nav and etc

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const elHamburger = document.querySelector("#jsHumBtn");
+const elNav = document.querySelector("#jsMobileNav");
+
+const hideNav = () => {
+  elNav.classList.toggle("mobile__navbar__active");
+};
+
+elHamburger.addEventListener("click", function (e) {
+  e.preventDefault;
+  elNav.classList.toggle("mobile__navbar__active");
+});
